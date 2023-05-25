38dce264d55ec28cadb4a922b3fc00d185a0f8e3
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2023-05-24 21:59:47 -0400

    se avanzó en el llamado a la API con la funcion fetch y se definieron los estilos y colores de fondo de la imagen del pokemon seleccionado

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const pokeCard = document.querySelector(['data-poke-card']);
-const pokeName = document.querySelector(['data-poke-name']);
-const pokeImg = document.querySelector(['data-poke-img']);
-const pokeImgContainer = document.querySelector(['data-poke-img-container']);
-const pokeId = document.querySelector(['data-poke-id']);
-const pokeTypes = document.querySelector(['data-poke-types']);
-const pokeStats = document.querySelector(['data-poke-stats']);
+const pokeCard = document.querySelector('[data-poke-card]');
+const pokeName = document.querySelector('[data-poke-name]');
+const pokeImg = document.querySelector('[data-poke-img]');
+const pokeImgContainer = document.querySelector('[data-poke-img-container]');
+const pokeId = document.querySelector('[data-poke-id]');
+const pokeTypes = document.querySelector('[data-poke-types]');
+const pokeStats = document.querySelector('[data-poke-stats]');
 
 const typeColors = {
     electric: '#FFEA70',
@@ -32,4 +32,27 @@ const searchPokemon = event =>{
     fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
         .then(data => data.json())
         .then(response => renderPokemonData(response))
+}
+
+const renderPokemonData= data =>{
+    const sprite= data.sprites.front_default;
+    const { stats, types } = data;
+    console.log(data);
+
+    pokeName.textContent = data.name;
+    pokeImg.setAttribute('src', sprite);
+    pokeId.textContent= `N° ${data.id}`;
+    setCardColor(types);
+    renderPokemonTypes(types);
+}
+
+const setCardColor = types =>{
+    const colorOne = typeColors[types[0].type.name];
+    const colorTwo = types[1] ? typeColors[types[1].type.name]: typeColors.default;
+    pokeImg.style.background = `radial-gradient(${colorTwo} 33%, ${colorOne} 33%)`;
+    pokeImg.style.backgroundSize= ' 5px, 5px';
+}
+
+renderPokemonTypes=types =>{
+    pokeTypes.innerHTML
 }
